eea0b05704752c46b7fdce11a52c4898c9acd9b0
Dawid Ruth <dawidolinp1@gmail.com>
2024-04-04 02:46:19 +0200

    create integration tests project

---
+++ ZadanieRekrutacyjne.IntegrationTests/TagsControllerTests.cs
@@ -0,0 +1,35 @@
+﻿
+using Xunit;
+using Moq;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+
+namespace ZadanieRekrutacyjne.IntegrationTests
+{
+    public class TagsControllerTests
+    {
+        [Fact]
+        public void GetAll_WithQueryParameteres_ReturnsOkResult()
+        {
+            //arange
+
+            var factory = new WebApplicationFactory<Program>();
+            var client = factory.CreateClient();
+
+
+
+            //act
+
+
+
+
+            //assert
+
+
+        }
+
+
+    }
+}
